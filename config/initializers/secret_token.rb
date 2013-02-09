d7e5e637797a6c98316bd0517de350373b6c7ffa
Nick Herbold <herboldn@gmail.com>
2013-02-09 16:21:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mets::Application.config.secret_token = '1e67ed076d71c3c60faaa5f920a7c181de9f64741339155870ffffd52a34b8acf32dae0e391e58352ec8f5f2ae15fa7a60a674abddbf52e080932278a45740b9'
